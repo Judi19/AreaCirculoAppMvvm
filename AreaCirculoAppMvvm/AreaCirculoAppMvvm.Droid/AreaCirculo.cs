fefa4ea53bf42893a20fcb8a20eb74cf79c469fa
Judi19 <167385372+Judi19@users.noreply.github.com>
2024-12-12 21:20:09 -0600

    Agregar archivos de proyecto.

---
+++ AreaCirculoAppMvvm/AreaCirculoAppMvvm.Droid/AreaCirculo.cs
@@ -0,0 +1,15 @@
+namespace AreaCirculoAppMvvm;
+
+public class AreaCirculo : ContentPage
+{
+	public AreaCirculo()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
---
+++ AreaCirculoAppMvvm/AreaCirculoAppMvvm/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AreaCirculoAppMvvm;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
